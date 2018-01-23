878a6fe18366a72e8ef014535fa03b92a0b16370
Pedram <abdzadehpedram@gmail.com>
2018-01-23 18:34:00 +0330

    create login page

--- src/appLayer/User.java
+++ src/appLayer/User.java
@@ -1,7 +1,14 @@
 package appLayer;
 
+import datalayer.DB_Student;
+
+import java.sql.SQLException;
+
 public class User {
-    public boolean isValidUser (String username, String password) {
-        return true;
+    public boolean isValidUser (String username, String password) throws SQLException, ClassNotFoundException
+    {
+        DB_Student student = new DB_Student();
+
+        return student.isValidUserLogin(username, password);
     }
 }
