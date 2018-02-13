98317b9cc27cd8a14df3f236e0416c07f7e4db54
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2018-02-13 14:02:33 +0330

    new changes again

--- src/webapp/Date.java
+++ src/webapp/Date.java
@@ -1,16 +1,23 @@
 package webapp;
 
 
-import com.ibm.icu.util.Calendar;
-import com.ibm.icu.util.PersianCalendar;
+//import com.ibm.icu.util.Calendar;
+//import com.ibm.icu.util.PersianCalendar;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class Date {
     public static String getCurrentTimeAndDate() {
-        PersianCalendar persianCalendar = new PersianCalendar(new java.util.Date());
-        int year = persianCalendar.get(Calendar.YEAR);
-        int month = persianCalendar.get(Calendar.MONTH) + 1;
-        int day = persianCalendar.get(Calendar.DAY_OF_MONTH);
-        String date = year + "-" + month + "-" + day;
+//        PersianCalendar persianCalendar = new PersianCalendar(new java.util.Date());
+//        int year = persianCalendar.get(Calendar.YEAR);
+//        int month = persianCalendar.get(Calendar.MONTH) + 1;
+//        int day = persianCalendar.get(Calendar.DAY_OF_MONTH);
+//        String date = year + "-" + month + "-" + day;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime now = LocalDateTime.now();
+        String date = dtf.format(now);
         return date;
     }
 }
